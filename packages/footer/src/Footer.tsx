580fd5c0cd8f89a1208e00a6890cbd1eeed2c7d1
Jordan Overbye <jordanoverbye@gmail.com>
2022-05-30 09:24:30 +1000

    Use token from core

--- packages/footer/src/Footer.tsx
+++ packages/footer/src/Footer.tsx
@@ -1,6 +1,7 @@
+import { tokens } from '@ag.ds-next/core';
 import { LinkList } from '@ag.ds-next/link-list';
-import { Footer as DSFooter, FooterDivider } from '@ag.ds-next/footer';
 import { Text } from '@ag.ds-next/text';
+import { Footer as AgDsFooter, FooterDivider } from '@ag.ds-next/footer';
 
 const footerLinks = [
 	{ href: '#', label: 'Accessibility' },
@@ -10,15 +11,15 @@ const footerLinks = [
 
 export function Footer() {
 	return (
-		<DSFooter variant="agriculture">
+		<AgDsFooter variant="agriculture">
 			<LinkList links={footerLinks} horizontal />
 			<FooterDivider />
-			<Text fontSize="xs" maxWidth="42em">
+			<Text fontSize="xs" maxWidth={tokens.maxWidth.bodyText}>
 				We acknowledge the traditional owners of country throughout Australia
 				and recognise their continuing connection to land, waters and culture.
 				We pay our respects to their Elders past, present and emerging.
 			</Text>
 			<Text fontSize="xs">&copy; Commonwealth of Australia.</Text>
-		</DSFooter>
+		</AgDsFooter>
 	);
 }
