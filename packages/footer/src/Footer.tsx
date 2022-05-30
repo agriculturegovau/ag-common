7c9e974217098df9ef84428a7d25a8db1881fd56
Peter <212986+dagoof@users.noreply.github.com>
2022-05-30 11:11:18 +1000

    feat(footer): use predefined links & CoreProvider (#1)

--- packages/footer/src/Footer.tsx
+++ packages/footer/src/Footer.tsx
@@ -2,24 +2,52 @@ import { tokens } from '@ag.ds-next/core';
 import { LinkList } from '@ag.ds-next/link-list';
 import { Text } from '@ag.ds-next/text';
 import { Footer as AgDsFooter, FooterDivider } from '@ag.ds-next/footer';
+import { CoreProvider } from '@ag.ds-next/core';
+import { useMemo } from 'react';
 
 const footerLinks = [
-	{ href: '#', label: 'Accessibility' },
-	{ href: '#', label: 'Disclaimer' },
-	{ href: '#', label: 'Privacy' },
+	{
+		href: '/about',
+		label: 'About',
+	},
+	{
+		href: '/account/help',
+		rel: 'external',
+		label: 'Help',
+	},
+	{
+		href: 'https://www.awe.gov.au/about/commitment/accessibility',
+		rel: 'external',
+		label: 'Accessibility',
+	},
+	{
+		href: 'https://www.awe.gov.au/about/disclaimer',
+		rel: 'external',
+		label: 'Disclaimer',
+	},
+	{
+		href: '/privacy',
+		label: 'Privacy',
+	},
 ];
 
-export function Footer() {
+export const Footer = () => {
+	const year = useMemo(() => new Date().getFullYear(), []);
+
 	return (
-		<AgDsFooter variant="agriculture">
-			<LinkList links={footerLinks} horizontal />
-			<FooterDivider />
-			<Text fontSize="xs" maxWidth={tokens.maxWidth.bodyText}>
-				We acknowledge the traditional owners of country throughout Australia
-				and recognise their continuing connection to land, waters and culture.
-				We pay our respects to their Elders past, present and emerging.
-			</Text>
-			<Text fontSize="xs">&copy; Commonwealth of Australia.</Text>
-		</AgDsFooter>
+		<CoreProvider>
+			<AgDsFooter variant="agriculture">
+				<LinkList links={footerLinks} horizontal />
+				<FooterDivider />
+				<Text fontSize="xs" maxWidth={tokens.maxWidth.bodyText}>
+					We acknowledge the traditional owners of country throughout Australia
+					and recognise their continuing connection to land, waters and culture.
+					We pay our respects to their Elders past, present and emerging.
+				</Text>
+				<Text fontSize="xs" maxWidth={tokens.maxWidth.bodyText}>
+					&copy; {year} Department of Agriculture, Water and the Environment
+				</Text>
+			</AgDsFooter>
+		</CoreProvider>
 	);
-}
+};
