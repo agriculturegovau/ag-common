7636d11569c175f9051ffa88e06fa129f62ec6b3
Jordan Overbye <jordanoverbye@gmail.com>
2022-06-27 12:37:04 +1000

    props

--- packages/header/src/Header.tsx
+++ packages/header/src/Header.tsx
@@ -1,16 +1,15 @@
-import { CoreProvider, tokens } from '@ag.ds-next/core';
+import { CoreProvider } from '@ag.ds-next/core';
 import { Header as AgDsHeader } from '@ag.ds-next/header';
 import { AvatarIcon } from '@ag.ds-next/icon';
-import { MainNav, MainNavLink } from '@ag.ds-next/main-nav';
+import { MainNav, MainNavButton } from '@ag.ds-next/main-nav';
 import { Logo } from '@ag.ds-next/ag-branding';
 
-interface HeaderProps {
-	authenticated?: boolean;
-	mode?: 'authenticated' | 'unauthenticated';
-	activePath: React.ComponentProps<typeof MainNav>['activePath'];
-	handleSignIn?: () => Promise<void>;
-	handleSignOut?: () => Promise<void>;
-}
+export type HeaderProps = {
+	authenticated: boolean;
+	activePath: string;
+	handleSignIn?: React.MouseEventHandler<HTMLButtonElement>;
+	handleSignOut?: React.MouseEventHandler<HTMLButtonElement>;
+};
 
 const authenticatedLinks = [
 	{
@@ -33,39 +32,39 @@ const authenticatedLinks = [
 
 export const Header = ({
 	authenticated,
-	mode = 'authenticated',
 	activePath,
 	handleSignIn,
 	handleSignOut,
-}: HeaderProps) => (
-	<CoreProvider>
-		<AgDsHeader
-			heading="Export Service"
-			subline="Supporting Australian agricultural exports"
-			logo={<Logo />}
-			variant="dark"
-			badgeLabel="beta"
-		/>
-
-		<MainNav
-			variant="agriculture"
-			links={mode === 'authenticated' ? authenticatedLinks : []}
-			activePath={activePath}
-			rightContent={
-				authenticated ? (
-					<MainNavLink
-						onClick={handleSignOut}
-						label="Sign out"
-						icon={AvatarIcon}
-					/>
-				) : (
-					<MainNavLink
-						onClick={handleSignIn}
-						label="Sign in"
-						icon={AvatarIcon}
-					/>
-				)
-			}
-		/>
-	</CoreProvider>
-);
+}: HeaderProps) => {
+	return (
+		<CoreProvider>
+			<AgDsHeader
+				heading="Export Service"
+				subline="Supporting Australian agricultural exports"
+				logo={<Logo />}
+				variant="dark"
+				badgeLabel="beta"
+			/>
+			<MainNav
+				variant="agriculture"
+				links={authenticated ? authenticatedLinks : []}
+				activePath={activePath}
+				rightContent={
+					authenticated ? (
+						<MainNavButton
+							onClick={handleSignOut}
+							label="Sign out"
+							icon={AvatarIcon}
+						/>
+					) : (
+						<MainNavButton
+							onClick={handleSignIn}
+							label="Sign in"
+							icon={AvatarIcon}
+						/>
+					)
+				}
+			/>
+		</CoreProvider>
+	);
+};
