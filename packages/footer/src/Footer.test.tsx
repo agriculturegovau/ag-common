0ba895280e6f5798bc96f42e538d622f55fd02a6
Jordan Overbye <6265154+jordanoverbye@users.noreply.github.com>
2023-01-11 10:04:50 +1000

    footer, header: Add tests (#32)

--- packages/footer/src/Footer.test.tsx
+++ packages/footer/src/Footer.test.tsx
@@ -1,7 +1,20 @@
+import '@testing-library/jest-dom';
+import 'html-validate/jest';
+import { render, cleanup } from '../../../test-utils';
 import { Footer } from './Footer';
 
+afterEach(cleanup);
+
 describe('Footer', () => {
-	it('exists', () => {
-		expect(Footer).toBeDefined();
+	it('renders correctly', () => {
+		const { container } = render(<Footer />);
+		expect(container).toMatchSnapshot();
+	});
+
+	it('renders a valid HTML structure', () => {
+		const { container } = render(<Footer />);
+		expect(container).toHTMLValidate({
+			extends: ['html-validate:recommended'],
+		});
 	});
 });
