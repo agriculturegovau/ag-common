4c48e77736fb7d5df07fe0fdda42a920cc8114b8
Jordan Overbye <jordanoverbye@gmail.com>
2023-07-11 11:42:04 +1000

    add beta badge

--- packages/app-layout/src/AppLayout.tsx
+++ packages/app-layout/src/AppLayout.tsx
@@ -56,6 +56,7 @@ export function AppLayout({
 					href="/"
 					heading="Export Service"
 					subLine="Supporting Australian agricultural exports"
+					badgeLabel="Beta"
 					logo={<Logo />}
 					accountDetails={{
 						href: '/account',
