3d8080395e37f2fce16c8923106ea7070cd5ba69
Peter DeGroff <212986+dagoof@users.noreply.github.com>
2023-12-19 16:50:46 +1100

    help-reference: use REST API instead of graphql

--- packages/help-reference/src/HelpReference.tsx
+++ packages/help-reference/src/HelpReference.tsx
@@ -1,5 +1,4 @@
 import { useContext, useState } from 'react';
-import request, { gql } from 'graphql-request';
 import useSWR from 'swr';
 import { Details } from '@ag.ds-next/react/details';
 import { Drawer } from '@ag.ds-next/react/drawer';
@@ -14,9 +13,10 @@ import {
 import { Text } from '@ag.ds-next/react/text';
 
 import { DocumentRenderer } from './DocumentRenderer';
-import { HelpArticleT, HelpReferenceT } from './types';
+import { HelpArticleT, articleDecoder, referenceDecoder } from './types';
 import { HelpReferenceContext } from './HelpReferenceContext';
 import { tokens } from '@ag.ds-next/react/core';
+import { Decoder, decode } from './decoder';
 
 type HelpReferenceProps = {
 	reference: string;
@@ -28,56 +28,31 @@ type ArticleLinkProps = {
 
 type SlugParam = { slug: string };
 
-const getReferenceQuery = gql`
-	query getReference($slug: String!) {
-		reference: reference(slug: $slug) {
-			slug
-			label
-			content
-			referenceText
-			articleSlug
-
-			article {
-				slug
-				title
-				intro
-				content
-			}
-		}
-	}
-`;
-
-const getArticleQuery = gql`
-	query getArticle($slug: String!) {
-		article: article(slug: $slug) {
-			slug
-			title
-			intro
-			content
-		}
-	}
-`;
-
-const useQuery = <T,>(document: string, variables: SlugParam) => {
+const useQuery = <T,>(
+	decoder: Decoder<T>,
+	resource: 'references' | 'articles',
+	variables: SlugParam
+) => {
 	const { providerURL } = useContext(HelpReferenceContext);
-	return useSWR({ document, variables }, ({ document, variables }) =>
-		request<T, SlugParam>({
-			url: `${providerURL}/api/graphql`,
-			document,
-			variables,
-		})
+	return useSWR(variables, ({ slug }) =>
+		fetch(`${providerURL}/api/help/${resource}/${slug}`)
+			.then((res) => (res.ok ? res.json() : Promise.reject('error')))
+			.then((data) => {
+				const t = decode(decoder, data);
+				return t.status === 'ok' ? t.t : Promise.reject(t.msg);
+			})
 	);
 };
 
 export const ArticleLink = ({ article, ...props }: ArticleLinkProps) => {
 	const [expanded, setExpanded] = useState(false);
 	const { providerURL } = useContext(HelpReferenceContext);
-	const { data } = useQuery<{ article: HelpArticleT | null }>(getArticleQuery, {
+	const { data } = useQuery(articleDecoder, 'articles', {
 		slug: article,
 	});
 
 	const href = `${providerURL}/help/page/${article}`;
-	const t = data?.article;
+	const t = data;
 	if (!t) {
 		return <TextLinkExternal {...props} href={href} />;
 	}
@@ -106,15 +81,16 @@ export const ArticleLink = ({ article, ...props }: ArticleLinkProps) => {
 export const HelpReference = (props: HelpReferenceProps) => {
 	const [expanded, setExpanded] = useState(false);
 	const { providerURL } = useContext(HelpReferenceContext);
-	const { data } = useQuery<{ reference: HelpReferenceT | null }>(
-		getReferenceQuery,
-		{ slug: props.reference }
-	);
+	const { data } = useQuery(referenceDecoder, 'references', {
+		slug: props.reference,
+	});
 
-	const reference = data?.reference;
-	if (!reference) return null;
+	const reference = data;
+	if (!reference) {
+		return null;
+	}
 
-	const href = `${providerURL}/help/page/${reference.articleSlug}`;
+	const href = `${providerURL}/help/page/${reference.article}`;
 
 	return (
 		<>
@@ -122,7 +98,7 @@ export const HelpReference = (props: HelpReferenceProps) => {
 				<Prose>
 					<DocumentRenderer document={reference.content ?? []} />
 					<p>
-						{reference.article ? (
+						{reference.helpArticle ? (
 							<TextLink
 								href={href}
 								onClick={(event) => {
@@ -141,9 +117,10 @@ export const HelpReference = (props: HelpReferenceProps) => {
 					</p>
 				</Prose>
 			</Details>
-			{reference.article ? (
+
+			{reference.helpArticle ? (
 				<HelpDrawer
-					article={reference.article}
+					article={reference.helpArticle}
 					href={href}
 					dismiss={() => setExpanded(false)}
 					showing={expanded}
--- packages/help-reference/src/types.tsx
+++ packages/help-reference/src/types.tsx
@@ -1,3 +1,14 @@
+import {
+	Decoder,
+	arrayDecoder,
+	jsonDecoder,
+	mapDecoder,
+	nullDecoder,
+	objDecoder,
+	oneOfDecoders,
+	stringDecoder,
+	undefinedDecoder,
+} from './decoder';
 import { Element } from './renderer';
 
 export type DocumentElement = Element;
@@ -12,9 +23,34 @@ export type HelpArticleT = {
 export type HelpReferenceT = {
 	slug: string;
 	label: string;
-	articleSlug: string;
+	article: string;
 	referenceText: string;
 	content: DocumentElement[];
-
-	article: HelpArticleT | null;
+	helpArticle?: HelpArticleT | null;
 };
+
+const documentElementDecoder = arrayDecoder(
+	mapDecoder(jsonDecoder, (v) => v as DocumentElement)
+);
+
+const dataDecoder = <T,>(decoder: Decoder<T>) =>
+	mapDecoder(objDecoder({ data: decoder }), (t) => t.data);
+
+const articleDecoder_ = objDecoder({
+	slug: stringDecoder,
+	title: stringDecoder,
+	intro: documentElementDecoder,
+	content: documentElementDecoder,
+});
+
+export const articleDecoder = dataDecoder(articleDecoder_);
+export const referenceDecoder = dataDecoder(
+	objDecoder({
+		slug: stringDecoder,
+		label: stringDecoder,
+		article: stringDecoder,
+		referenceText: stringDecoder,
+		content: documentElementDecoder,
+		helpArticle: oneOfDecoders(articleDecoder_, nullDecoder, undefinedDecoder),
+	})
+);
