5c0865c4989de6b53ebeeec05c9cc2aa109842cc
dagoof <dagoof@users.noreply.github.com>
2024-01-25 03:27:41 +0000

    Deploy preview for PR 81 🛫

---
+++ pr-preview/pr-81/assets/syntaxhighlighter-4OVWVELQ-918fa933.js
@@ -0,0 +1,2 @@
+import{S as i,c as e,s as p}from"./DocsRenderer-3PUGWF3O-a7393e66.js";import"./iframe-acb60f67.js";import"../sb-preview/runtime.js";export{i as SyntaxHighlighter,e as createCopyToClipboardFunction,p as default};
+//# sourceMappingURL=syntaxhighlighter-4OVWVELQ-918fa933.js.map
