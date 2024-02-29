58cc7586c44defb47e520763d8f15ee9a6b3b24c
Peter DeGroff <212986+dagoof@users.noreply.github.com>
2024-02-29 14:00:39 +1100

    rename var

--- packages/help-callout/src/HelpCallout.tsx
+++ packages/help-callout/src/HelpCallout.tsx
@@ -4,38 +4,38 @@ import { Text } from '@ag.ds-next/react/text';
 import { TextLink, TextLinkExternal } from '@ag.ds-next/react/text-link';
 
 interface HelpCalloutProps {
-	hideHelpArticles?: boolean;
-	helpHref?: string;
-	internal?: boolean;
+  hideHelpArticles?: boolean;
+  helpHref?: string;
+  internal?: boolean;
 }
 
 export const HelpCallout = (props: HelpCalloutProps) => {
-	const Linker = props?.internal ? TextLink : TextLinkExternal;
+  const LinkComponent = props?.internal ? TextLink : TextLinkExternal;
 
-	return (
-		<CoreProvider>
-			<Callout
-				title={
-					props?.hideHelpArticles === true ? 'Need more help?' : 'Need help?'
-				}
-			>
-				{props?.hideHelpArticles === true ? null : (
-					<Text>
-						Search our <Linker href={props.helpHref ?? '/help'}>Help</Linker>{' '}
-						pages
-					</Text>
-				)}
-				<Text>
-					Email{' '}
-					<TextLink href="mailto:exportservice@aff.gov.au">
-						exportservice@aff.gov.au
-					</TextLink>
-				</Text>
-				<Text>
-					Call <TextLink href="tel:1800571125">1800&nbsp;571&nbsp;125</TextLink>
-					, Monday to Friday, 9 am to 5 pm AEST
-				</Text>
-			</Callout>
-		</CoreProvider>
-	);
+  return (
+    <CoreProvider>
+      <Callout
+        title={
+          props?.hideHelpArticles === true ? 'Need more help?' : 'Need help?'
+        }
+      >
+        {props?.hideHelpArticles === true ? null : (
+          <Text>
+            Search our <LinkComponent href={props.helpHref ?? '/help'}>Help</Linker>{' '}
+            pages
+          </Text>
+        )}
+        <Text>
+          Email{' '}
+          <TextLink href="mailto:exportservice@aff.gov.au">
+            exportservice@aff.gov.au
+          </TextLink>
+        </Text>
+        <Text>
+          Call <TextLink href="tel:1800571125">1800&nbsp;571&nbsp;125</TextLink>
+          , Monday to Friday, 9 am to 5 pm AEST
+        </Text>
+      </Callout>
+    </CoreProvider>
+  );
 };
