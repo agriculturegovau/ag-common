1e983f7fee05cd3e51770bfaa9bc15ae73ecd7c3
dagoof <dagoof@users.noreply.github.com>
2024-03-20 01:38:45 +0000

    Deploy preview for PR 90 🛫

---
+++ pr-preview/pr-90/assets/chunk-MSDTXLE7-deaff06e.js
@@ -0,0 +1,2 @@
+import{r as a}from"./iframe-be2e3180.js";var l=({icon:e,color:t,size:o,...i})=>{const n=t||"currentColor",r=o||"14px";return a.createElement("span",{role:"img","aria-hidden":"true",style:{color:n,width:r,height:r,display:"inline-flex",fontSize:"inherit"},...i},e)};export{l as I};
+//# sourceMappingURL=chunk-MSDTXLE7-deaff06e.js.map
