3d5d70460075f93678ac4eaf98d494db94140ced
Mel <presstocontinue@gmail.com>
2025-05-20 16:30:06 +1000

    update babel test

--- babel.config.js
+++ babel.config.js
@@ -1,32 +1,8 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const emotionPreset = require('@emotion/babel-preset-css-prop').default;
-
-module.exports = (api) => {
-	const isTest = api.env('test');
-
-	if (isTest) {
-		return {
-			presets: [
-				[
-					'next/babel',
-					{
-						'preset-react': {
-							runtime: 'automatic',
-							importSource: '@emotion/react',
-						},
-					},
-				],
-			],
-			plugins: ['@emotion/babel-plugin'],
-		};
-	}
-
-	return {
-		presets: [
-			['@babel/preset-env', { targets: { node: 'current' } }],
-			'@babel/preset-typescript',
-			emotionPreset,
-		],
-		plugins: ['@emotion/babel-plugin'],
-	};
+module.exports = {
+	presets: [
+		['@babel/preset-env', { targets: { node: 'current' } }],
+		'@babel/preset-typescript',
+		require('@emotion/babel-preset-css-prop').default ||
+			require('@emotion/babel-preset-css-prop'),
+	],
 };
