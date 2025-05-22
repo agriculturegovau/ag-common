6800704f16fbdaefce87c854931ca03ace6e0cb6
Mel <presstocontinue@gmail.com>
2025-05-22 13:19:55 +1000

    update babel for storybook to use regular requirment

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,15 @@
-module.exports = {
-	presets: [
-		['@babel/preset-env', { targets: { node: 'current' } }],
-		'@babel/preset-typescript',
-		require('@emotion/babel-preset-css-prop').default ||
-			require('@emotion/babel-preset-css-prop'),
-	],
+module.exports = () => {
+	const isStorybook = process.env.STORYBOOK === 'true';
+
+	return {
+		presets: [
+			['@babel/preset-env', { targets: { node: 'current' } }],
+			'@babel/preset-typescript',
+			// Use string format for Storybook, require for other builds
+			isStorybook
+				? '@emotion/babel-preset-css-prop'
+				: require('@emotion/babel-preset-css-prop').default ||
+					require('@emotion/babel-preset-css-prop'),
+		],
+	};
 };
