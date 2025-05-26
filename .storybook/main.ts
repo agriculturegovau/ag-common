ea1305fa90ead0a600e49bb546d6e40fbac7468b
Mel <presstocontinue@gmail.com>
2025-05-26 13:06:24 +1000

    update lint

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,32 +1,31 @@
 import { dirname, join } from 'path';
 import type { StorybookConfig } from '@storybook/react-vite';
 
+function getAbsolutePath(value: string): string {
+	return dirname(require.resolve(join(value, 'package.json')));
+}
+
 const config: StorybookConfig = {
-    framework: {
-		name: getAbsolutePath("@storybook/react-vite"),
+	framework: {
+		name: getAbsolutePath('@storybook/react-vite'),
 		options: {},
 	},
-
-    addons: [
+	addons: [
 		getAbsolutePath('@storybook/addon-links'),
 		getAbsolutePath('@storybook/addon-a11y'),
-		{ name: '@storybook/addon-essentials', options: { background: false } },
-		getAbsolutePath("@chromatic-com/storybook"),
+		{
+			name: '@storybook/addon-essentials',
+			options: { background: false },
+		},
+		getAbsolutePath('@chromatic-com/storybook'),
 	],
-
-    stories: ['../packages/*/src/**/*.stories.@(ts|tsx)'],
-
-    typescript: {
+	stories: ['../packages/*/src/**/*.stories.@(ts|tsx)'],
+	typescript: {
 		reactDocgen: 'react-docgen-typescript',
 	},
-
-    docs: {
-        autodocs: true
-    }
+	docs: {
+		autodocs: true,
+	},
 };
 
 export default config;
-
-function getAbsolutePath(value: string): any {
-	return dirname(require.resolve(join(value, 'package.json')));
-}
