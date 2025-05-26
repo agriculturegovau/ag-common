3f6f42ee84cd227c448009728d51ba59bc40c254
Mel <presstocontinue@gmail.com>
2025-05-26 13:41:52 +1000

    update lint

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,11 @@
-module.exports = () => {
+module.exports = (api) => {
+	api.cache(true);
+
 	return {
 		presets: [
 			['@babel/preset-env', { targets: { node: 'current' } }],
 			'@babel/preset-typescript',
 			'@emotion/babel-preset-css-prop',
 		],
-		plugins: ['@emotion/babel-plugin'],
 	};
 };
