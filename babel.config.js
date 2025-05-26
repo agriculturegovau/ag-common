e8bbd67676c00b41a16e762b9372432f7261128f
Mel <presstocontinue@gmail.com>
2025-05-26 13:08:49 +1000

    update lint

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,31 @@
-module.exports = (api) => {
-	api.cache(true);
+module.exports = ({ env }) => {
+	const isTest = env() === 'test';
 
+	// This babel config is used for our component tests (jest, react-testing-library)
+	if (isTest) {
+		return {
+			presets: [
+				[
+					'next/babel',
+					{
+						'preset-react': {
+							runtime: 'automatic',
+							importSource: '@emotion/react',
+						},
+					},
+				],
+			],
+			plugins: ['@emotion/babel-plugin'],
+		};
+	}
+
+	// This babel config is used for everything else which include: NPM package bundles, docs/template site (nextjs), storybook and playroom
 	return {
 		presets: [
 			['@babel/preset-env', { targets: { node: 'current' } }],
 			'@babel/preset-typescript',
 			'@emotion/babel-preset-css-prop',
 		],
+		plugins: ['@emotion/babel-plugin'],
 	};
 };
